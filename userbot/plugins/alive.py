ec1fa88f21a346650bd70b51e19525701fa74259
nikhilcroaker <nikdoper@gmail.com>
2020-10-30 19:04:02 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,24 +4,24 @@ from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME, hellversion
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
-PM_IMG = "https://telegra.ph/file/f34675b4e94d4290c0b6b.mp4"
-pm_caption = "🔥🔥**Hêllẞø† IS ONLINE🔥🔥\n\n\n"
+PM_IMG = "https://telegra.ph/file/643ce03c1e9dc3f73938a.mp4"
+pm_caption = "🔥🔥**Nɪᴛʀɪᴄ Bᴏᴛ Is Oɴ Tᴀʙᴀʜɪ🔥🔥\n\n\n"
 
-pm_caption += f"⚔️⚔️**MASTER**⚔️⚔️       : {DEFAULTUSER}\n\n"
+pm_caption += f"🎌 Bᴏᴛ Pᴏᴡᴇʀᴇᴅ ʙʏ        : {DEFAULTUSER}\n\n"
 
-pm_caption += "🛡️🛡️**TELETHON**🛡️🛡️   : 1.15.0 \n\n"
+pm_caption += "🎌 Tᴇʟᴇᴛʜᴏɴ               : 1.15.0 \n\n"
 
-pm_caption += f"😈😈**Hêllẞø†**😈😈         : `{hellversion}`\n\n"
+pm_caption += f"🎌 Vᴇʀsɪᴏɴ               : `{hellversion}`\n\n"
 
-pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n\n"
+pm_caption += "🌐 Cʜᴀɴɴᴇʟ                : Jᴏɪɴ(https://t.me/NoobCheats)\n\n"
 
-pm_caption += "🔱🔱**GROUP**🔱🔱.         : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n\n"
+pm_caption += "🌠 Gʀᴏᴜᴘ                  : Jᴏɪɴ(https://t.me/NoobCheatSupport)\n\n"
 
-pm_caption += "😎😎**LICENSE**😎😎       : [ӀíϲҽղՏҽ](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)\n\n"
+pm_caption += "🎫 Lɪᴄᴇɴsᴇ                : Lɪᴄᴇɴsᴇ(https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)\n\n"
 
-pm_caption += "🔥🔥**CREATOR🔥🔥      : [HellBot-Owner](https://t.me/kraken_the_badass)\n\n"
+pm_caption += "🎭 Cʀᴇᴀᴛᴏʀ                : Bᴏᴛ Oᴡɴᴇʀ(https://t.me/Its_Nitric)\n\n"
 
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/hellbot_official)\n\n"
+
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
